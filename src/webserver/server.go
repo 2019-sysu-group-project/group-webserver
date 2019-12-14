e14cec78227ddacc78b2bbd41196410f500f712a
weimch <weimch3@qq.com>
2019-12-14 19:56:33 +0800

    add mysql and redis client support

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -1,93 +1,112 @@
 package main
 
 import (
+	"os"
+	"database/sql"
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+// redis 默认是没有密码和使用0号db
+var redis_client *redis.Client
+var mysql_client *sql.DB
+
+func init() {
+	redis_client = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:16379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	var err error
+	_, err = redis_client.Ping().Result()
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("Error open redis connection")
+		os.Exit(-1)
+	}
+	mysql_client, err = sql.Open("mysql", "root:123@tcp(127.0.0.1:13306)/projectdb")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+	_, err = mysql_client.Query("SELECT * FROM User")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+}
+
+// mysql 默认用户：root，密码：root，数据库：projectdb
+// var mysql_client =
+
 type Coupon struct {
-	username string
-	coupons string
-	amount int32
-	stock float32
-	left int32
+	username    string
+	coupons     string
+	amount      int32
+	stock       float32
+	left        int32
 	description string
 }
 
 // 任务1
-func registerUser(c *gin.Context){
+func registerUser(c *gin.Context) {
 	fmt.Println("This is registerUser")
 }
 
 // 任务1
-func validateJWT() bool{
+func validateJWT() bool {
 	// 需要编写JWT的验证机制，作为其他人能调用的一部分
 	return true
 }
 
 // 任务1
-func userLogin(c *gin.Context){
-	
+func userLogin(c *gin.Context) {
+
 }
 
 // 任务2
-func createCoupons(c *gin.Context){
-	
+func createCoupons(c *gin.Context) {
+
 }
 
 // 任务2
-func getCouponsInformation(c *gin.Context){
-	
+func getCouponsInformation(c *gin.Context) {
+
 }
 
 // 任务2
-func getCouponsFromRedisOrDatabase(username string, coupons string) Coupon{
+func getCouponsFromRedisOrDatabase(username string, coupons string) Coupon {
 	return Coupon{}
 }
 
-// 任务2
-func initRedis(){
-	// 这只是Redis初始化的例子，只是我测试用的，你需要自己修改相应部分，默认是没有密码和使用0号db
-	client := redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:16379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	pong, _ := client.Ping().Result()
-	client.Set("key", "2333", 0)
-	result, _ := client.Get("key").Result()
-	fmt.Println(pong)
-	fmt.Println(result)
-}
-
 // 任务3
-func setCouponsToRedisAndDatabase(coupon Coupon) bool{
+func setCouponsToRedisAndDatabase(coupon Coupon) bool {
 	// true set成功，false set失败
 	return true
 }
 
 // 任务3
-func patchCoupons(c *gin.Context){
-	
+func patchCoupons(c *gin.Context) {
+
 }
 
-func main()  {
-	initRedis()
+func main() {
 	// gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	
+
 	router.PATCH("/api/users/:username/coupons/:name", patchCoupons)
 	router.POST("/api/users", registerUser)
-	
+
 	router.POST("/api/auth", userLogin)
 	router.POST("/api/users/:username/coupons", createCoupons)
-	
+
 	router.GET("/api/users/:username/coupons", getCouponsInformation)
 
 	err := router.Run(":8080")
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error starting server")
 	}
 }
