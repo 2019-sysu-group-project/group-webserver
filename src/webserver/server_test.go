98fbf1a1dbb23a5f2e2d42df9a6e177496c9ece6
starthemoon <812138420@qq.com>
2019-12-19 21:50:21 +0800

    mine

--- src/webserver/server_test.go
+++ src/webserver/server_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"net/http"
 	"github.com/gin-gonic/gin"
+
 )
 
 // 任务1
@@ -30,9 +31,25 @@ func testGetCouponsInformation(t *testing.T) {
 }
 
 // 任务3
-func testPatchCoupons(t *testing.T) {
-	
+// @Return
+// Param1: 返回1，若异常状态为5xx；否则返回0
+// Param2: 返回响应所耗时间
+// 待后续并发进程调用
+func testPatchCoupons(t *testing.T) (int, int) {
+	router := setupRouter()
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("Patch", "/api/users/:username/coupons/:name", nil)
-	
+	router.serveHTTP(w, req)
+	 // 提取响应
+	 result := w.Result()
+	 defer result.Body.Close()
+	 // 读取响应body
+	 body,_ := ioutil.ReadAll(result.Body)
+	var x int
+	if code / 500 > 0 {
+		x = 1
+	} else {
+		x = 0
+	}
+	body
 }
