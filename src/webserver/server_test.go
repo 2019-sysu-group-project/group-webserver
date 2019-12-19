4d50818a24bef2edc8dcb733c80c7a970b693ef4
ClassmateLi <57695619+ClassmateLi@users.noreply.github.com>
2019-12-19 21:01:06 +0800

    任务2

--- src/webserver/server_test.go
+++ src/webserver/server_test.go
@@ -16,28 +16,31 @@ func testRegisterUser(t *testing.T){
 func testUserLogin(t *testing.T){
 
 }
-
 //任务2
 func testCreateCoupons(t *testing.T){
 	router:=setupRouter()
-	cou:=Coupon{"root","food",10,3.2,10,""}
-	str_cou,_ := json.Marshal(cou)
-	req:=httptest.NewRequest("POST","/api/users/:username/coupons",bytes.NewBuffer(str_cou))
+	var cou CouponV2
+	cou=CouponV2{"root","food",10,3.2,10,"gff"}
+	strCou,_ := json.Marshal(cou)
+	byCou:=bytes.NewReader(strCou)
+	req:=httptest.NewRequest("POST","/api/users/:username/coupons",byCou)
 	w:=httptest.NewRecorder()
 	router.ServeHTTP(w,req)
 	assert.Equal(t,http.StatusOK,w.Code)
 }
 //任务2
-func testGetCouponsInformation(t*testing.T){
+func testGetCouponsInformation(t *testing.T){
 	router:=setupRouter()
-	req:=httptest.NewRequest("GET","/api/users/:username/coupons",bytes.NewBuffer(str_cou))
+	req:=httptest.NewRequest("GET","/api/users/:username/coupons?username=root&coupons=food",nil)
 	w:=httptest.NewRecorder()
 	router.ServeHTTP(w,req)
 	assert.Equal(t,http.StatusOK,w.Code)
+	var couv2 CouponV2
+	json.Unmarshal([]byte(w.Body.String()), &couv2)
 	var cou Coupon
-	err := json.Unmarshal([]byte(w.Body.String()), &cou)
-	assert.Equal(t,"",cou.username)
-	assert.Equal(t,"",cou.coupons)
+	cou=couv2.ToCoupon()
+	assert.Equal(t,"root",cou.username)
+	assert.Equal(t,"food",cou.coupons)
 }
 
 // 任务3
