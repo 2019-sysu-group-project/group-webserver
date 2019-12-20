e23b4e3b531826c8f1ec639639e7938f1ca7e2cc
weimch <weimch3@qq.com>
2019-12-20 15:40:57 +0800

    adding testing

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-	"time"
 	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
 	_ "github.com/go-sql-driver/mysql"
-	jwt "github.com/dgrijalva/jwt-go"
-
-	"crypto/md5"
-	"io"
-	"strconv"
 )
 
 // redis 默认是没有密码和使用0号db
@@ -32,9 +28,7 @@ func init() {
 		fmt.Println("Error open redis connection")
 		os.Exit(-1)
 	}
-	// mysql_client, err = sql.Open("mysql", "root:123@tcp(127.0.0.1:13306)/projectdb")
-	// test
-	mysql_client, err = sql.Open("mysql", "root:123@tcp(127.0.0.1:3306)/projectdb")
+	mysql_client, err = sql.Open("mysql", "root:123@tcp(127.0.0.1:13306)/projectdb")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
@@ -50,199 +44,51 @@ func init() {
 // var mysql_client =
 
 type Coupon struct {
-	username    string
-	coupons     string
-	amount      int32
-	stock       float32
-	left        int32
-	description string
+	Username    string
+	Coupons     string
+	Amount      int32
+	Stock       float32
+	Left        int32
+	Description string
 }
 
 type User struct {
-	Username	string
-	Password	string
-	Kind		string
+	Username string
+	Password string
+	Kind     string
 }
 
 type User_DB struct {
-	Username    string
-	Password    string
-	Kind 	    int
+	Username string
+	Password string
+	Kind     int
 }
-
-var(
-	key []byte = []byte("JWT key of GXNXFD")
-)
 
 // 任务1
 func registerUser(c *gin.Context) {
-	// fmt.Println("This is registerUser")
-	var json User
 
-	if c.BindJSON(&json) == nil {
-		username := json.Username
-		password := json.Password
-		kind := json.kind   	// string type
-		if isUserExist(username) {		// user already exists
-			c.JSON(400, gin.H{
-				"errMsg": "Username already exists!",
-			})
-			return
-		} else {
-			int_kind, err := strconv.Atoi(kind)
-			if err != nil {
-				c.JSON(400, gin.H{
-					"errMsg": "Post error."
-				})
-				return
-			}
-			passwordHash := md5Hash(password)
-			// insert user to DB
-			if insertUser(username, passwordHash, int_kind) {
-				c.JSON(201, gin.H{
-					"errMsg": "",
-				})
-				return
-			} else {
-				c.JSON(400, gin.H{
-					"errMsg": "Create user failed!",
-				})
-				return
-			}
-		}
-	} else {		// failed in BindJSON
-		c.JSON(400, gin.H{
-			"errMsg": "Failed in BindJSON!",
-		})
-		return
-	}
-}
-
-// generate JWT token
-func genToken() string {
-	claims := &jwt.StandardClaims{
-		NotBefore: int64(time.Now().Unix()),
-		ExpiresAt: int64(time.Now().Unix() + 3600),
-		Issuer:    "GXNXFD",
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString(key)
-	if err != nil {
-		return ""
-	}
-	return ss
 }
 
 // 任务1
 func validateJWT(c *gin.Context) bool {
 	// 需要编写JWT的验证机制，作为其他人能调用的一部分
-	token := c.Request.Header.Get("Authorization")
-	_, err := jwt.Parse(token, func(*jwt.Token) (interface{}, error) {
-		return key, nil
-	})
-	if err != nil {
-		return false
-	}
 	return true
 }
 
 // 任务1
 func userLogin(c *gin.Context) {
-	var json User
-	if c.BindJSON(&json) == nil {
-		if authenticateUser(json.Username, json.Password) {
-			token := genToken()
-			if token == "" {
-				c.JSON(401, gin.H{
-					"kind": "",
-					"errMsg": "Generate token failed.",
-				})
-				return
-			} else {
-				var user User_DB
-				err := mysql_client.QueryRow("SELECT kind FROM User WHERE username=?", json.Username).Scan(&user.Kind)
-				if err != nil {
-					c.JSON(500, gin.H{
-						"kind": "",
-						"errMsg": "Query DB failed.",
-					})
-					return
-				} else {
-					string_kind := strconv.Itoa(user.Kind)
-					c.JSON(200, gin.H{
-						"Authorization": token,
-						"kind": string_kind,
-						"errMsg": "",
-					})
-					return
-				}
-			}
-		} else {
-			c.JSON(401, gin.H{
-				"kind": "",
-				"errMsg": "Login failed.",
-			})
-			return
-		}
-	} else {			// failed in BindJSON
-		c.JSON(400, gin.H{
-			"errMsg": "Failed in BindJSON!",
-		})
-		return
-	}
+
 }
 
 // check if the user already exists in DB
 func isUserExist(query_username string) bool {
 	var user User_DB
 	err := mysql_client.QueryRow("SELECT username, password, kind FROM User WHERE username=?", query_username).Scan(&user.Username, &user.Password, &user.Kind)
-	if err == sql.ErrNoRows {		// user not exists
+	if err == sql.ErrNoRows { // user not exists
 		return false
 	} else {
 		return true
 	}
-}
-
-// md5
-func md5Hash(data string) string {
-	hash := md5.New()
-	io.WriteString(hash, data)
-	return fmt.Sprintf("%x", hash.Sum(nil))
-}
-
-// insert user into DB
-func insertUser(username string, password string, kind int) bool {
-	result, err := mysql_client.Exec("INSERT INTO User(username, password, kind) VALUES(?,?,?)", username, password, kind)
-	if err != nil {
-		// insert failed
-		return false
-	}
-	_, err = result.LastInsertId()
-	if err != nil {
-		return false
-	}
-	_, err = result.RowsAffected()
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-// authenticate user from DB
-func authenticateUser(username string, password string) bool {
-	passwordHash := md5Hash(password)
-	var user User
-	err := mysql_client.QueryRow("SELECT username, password FROM User WHERE username=?", username).Scan(&user.Username, &user.Password)
-	if err == sql.ErrNoRows {
-		return false
-	} else {
-		if user.Username == username {
-			if user.Password == passwordHash {
-				return true
-			}
-		}
-	}
-	return false
 }
 
 // 任务2
@@ -255,44 +101,50 @@ func getCouponsInformation(c *gin.Context) {
 
 }
 
-// 任务2
-func getCouponsFromRedis(username string, coupons string) Coupon {
-	return Coupon{}
-}
-
-// 任务2
-func setCouponsToRedis(usernam string, coupons Coupon) {
-
-}
-
-// 任务2
-func getCouponsFromRedisOrDatabase(username string, coupons string) Coupon {
-	return Coupon{}
-}
-
-// 任务3 - 使用getCouponsFromRedis和setCouponsToRedis来完成该任务
-func setCouponsToRedisAndDatabase(coupon Coupon) bool {
-	// true set成功，false set失败
-	return true
-}
-
 // 任务3
 func patchCoupons(c *gin.Context) {
 
 }
 
-func setupRouter() *gin.Engine{
+func setupRouter() *gin.Engine {
 	router := gin.Default()
-	router.PATCH("/api/users/:username/coupons/:name", patchCoupons)
+	// task1
 	router.POST("/api/users", registerUser)
-
 	router.POST("/api/auth", userLogin)
+	// task2
 	router.POST("/api/users/:username/coupons", createCoupons)
-
 	router.GET("/api/users/:username/coupons", getCouponsInformation)
+	// task3
+	router.PATCH("/api/users/:username/coupons/:name", patchCoupons)
+
+	// used for testing
+	router.GET("/validate", testValidateJWT)
+	router.GET("/test", testMyFunc)
 	return router
 }
 
+func testMyFunc(c *gin.Context) {
+	s := `{"errMsg": "", "data": [{"name": "test_coupons_xxx", "amount": 100, "left": 30, "stock": 500, "description": "no description"}]}`
+	var result map[string]interface{}
+	err := json.Unmarshal([]byte(s), &result)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	c.JSON(200, result)
+}
+
+func testValidateJWT(c *gin.Context) {
+	valid := validateJWT(c)
+	if valid {
+		c.JSON(200, gin.H{
+			"errMsg": "valid",
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"errMsg": "invalid",
+		})
+	}
+}
 
 func main() {
 	// gin.SetMode(gin.ReleaseMode)
--- src/webserver/server_test.go
+++ src/webserver/server_test.go
@@ -1,32 +1,258 @@
 package main
 
 import (
-	// "net/http"
-	// "net/http/httptest"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// 任务1
-func testRegisterUser(t *testing.T){
-	
+type GeneralResult struct {
+	Kind   string `json:"kind"`
+	ErrMsg string `json:"errMsg"`
+}
+
+type TestCouponsResponse struct {
+	ErrMsg string            `json:"errMsg"`
+	Data   []TestCouponsData `json:"data"`
+}
+
+type TestCouponsData struct {
+	Name        string `json:"name"`
+	Amount      int32  `json:"amount"`
+	Left        int32  `json:"left"`
+	Stock       int32  `json:"stock"`
+	Description string `json:"description"`
 }
 
 // 任务1
-func testUserLogin(t *testing.T){
+func TestRegisterUser(t *testing.T) {
+	var result GeneralResult
+	router := setupRouter()
+	// 创建客户
+	w := httptest.NewRecorder()
+	jsonStr := []byte(`{"username": "customer-test", "password": "123", "kind": "customer"}`)
+	req, _ := http.NewRequest("POST", "/api/users", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 201, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "", result.ErrMsg)
+	// 不填kind类型——默认创建客户
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"username": "customer-test1", "password": "123"}`)
+	req, _ = http.NewRequest("POST", "/api/users", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 201, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "", result.ErrMsg)
+	// 创建商家
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"username": "saler-test", "password": "123456", "kind": "saler"}`)
+	req, _ = http.NewRequest("POST", "/api/users", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 201, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "", result.ErrMsg)
+	// 检查新用户是否创建成功
+	assert.True(t, isUserExist("customer-test"))
+	assert.True(t, isUserExist("customer-test1"))
+	assert.True(t, isUserExist("saler-test"))
+	// 重复创建新用户
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"username": "customer-test", "password": "123", "kind": "customer"}`)
+	req, _ = http.NewRequest("POST", "/api/users", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "用户已存在", result.ErrMsg)
+	// 使用错误的kind类型，创建新用户
+	jsonStr = []byte(`{"username": "customer-test", "password": "123", "kind": "customerxxx"}`)
+	req, _ = http.NewRequest("POST", "/api/users", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "错误kind类型", result.ErrMsg)
+	// 使用空用户名创建用户
+	jsonStr = []byte(`{"username": "", "password": "123", "kind": "customerxxx"}`)
+	req, _ = http.NewRequest("POST", "/api/users", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "空用户名或密码", result.ErrMsg)
+	// 使用空的密码创建用户
+	jsonStr = []byte(`{"username": "customer-test", "password": "", "kind": "customerxxx"}`)
+	req, _ = http.NewRequest("POST", "/api/users", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "空用户名或密码", result.ErrMsg)
+}
 
+// 任务1
+func TestUserLogin(t *testing.T) {
+	var result GeneralResult
+	var header string
+	router := setupRouter()
+	// 空用户和空密码认证
+	w := httptest.NewRecorder()
+	jsonStr := []byte(`{"username": "", "password": ""}`)
+	req, _ := http.NewRequest("POST", "/api/auth", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 401, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "空用户或空密码", result.ErrMsg)
+	// 不存在的用户认证
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"username": "no-exist", "password": "123"}`)
+	req, _ = http.NewRequest("POST", "/api/auth", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 401, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "用户不存在", result.ErrMsg)
+	// 错误密码认证
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"username": "customer-test", "password": "1234"}`)
+	req, _ = http.NewRequest("POST", "/api/auth", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 401, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "错误密码", result.ErrMsg)
+	// 客户认证
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"username": "customer-test", "password": "123"}`)
+	req, _ = http.NewRequest("POST", "/api/auth", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "customer", result.Kind)
+	assert.Equal(t, "", result.ErrMsg)
+	header = w.Header().Get("Authorization")
+	assert.NotEqual(t, "", header) // 非空检查
+	// 检查是否Authorization
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/validate", nil)
+	req.Header.Set("Authorization", header)
+	router.ServeHTTP(w, req)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "valid", result.ErrMsg)
+	// 商家认证
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"username": "saler-test", "password": "123456"}`)
+	req, _ = http.NewRequest("POST", "/api/auth", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "saler", result.Kind)
+	assert.Equal(t, "", result.ErrMsg)
+	header = w.Header().Get("Authorization")
+	assert.NotEqual(t, "", header) // 非空检查
+	// 检查是否Authorization
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/validate", nil)
+	req.Header.Set("Authorization", header)
+	router.ServeHTTP(w, req)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "valid", result.ErrMsg)
 }
 
 // 任务2
-func testCreateCoupons(t *testing.T){
+func TestCreateCoupons(t *testing.T) {
+	var result GeneralResult
+	router := setupRouter()
+	// 商家taobao创建优惠券coupons_xxx
+	w := httptest.NewRecorder()
+	jsonStr := []byte(`{"name": "coupons_xxx", "amount": 100, "description": "xxx", "stock": 500}`)
+	req, _ := http.NewRequest("POST", "/api/users/taobao/coupons", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 201, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "", result.ErrMsg)
+	// 不存在的商家新建优惠券
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"name": "coupons_xxx", "amount": 100, "description": "xxx", "stock": 500}`)
+	req, _ = http.NewRequest("POST", "/api/users/no-exist/coupons", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "不存在的商家", result.ErrMsg)
+	// 非商家用户新建优惠券
+	w = httptest.NewRecorder()
+	jsonStr = []byte(`{"name": "coupons_xxx", "amount": 100, "description": "xxx", "stock": 500}`)
+	req, _ = http.NewRequest("POST", "/api/users/alice/coupons", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	result = GeneralResult{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, "非商家不能创建优惠券", result.ErrMsg)
+}
 
+func TestSample(t *testing.T) {
+	var result TestCouponsResponse
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/test", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	err := json.NewDecoder(w.Body).Decode(&result)
+	assert.Nil(t, err)
+	assert.Equal(t, result.Data[0].Name, "test_coupons_xxx")
 }
 
 // 任务2
-func testGetCouponsInformation(t *testing.T){
-
+func TestGetCouponsInformation(t *testing.T) {
+	var result TestCouponsResponse
+	router := setupRouter()
+	// 得到taobao商家创建的优惠券列表
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/users/taobao/coupons?page=1", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	result = TestCouponsResponse{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, 3, len(result.Data))
+	// 得到bob用户获得的优惠券列表
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/api/users/bob/coupons?page=1", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	result = TestCouponsResponse{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, 1, len(result.Data))
+	assert.Equal(t, result.Data[0].Amount, 1)
+	assert.Equal(t, result.Data[0].Stock, 700)
+	assert.Equal(t, result.Data[0].Left, 1)
+	assert.Equal(t, result.Data[0].Name, "clothes")
+	assert.Equal(t, result.Data[0].Description, "")
+	// 不存在用户的获得优惠券列表
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/api/users/non-exist/coupons?page=1", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	result = TestCouponsResponse{}
+	assert.Nil(t, json.NewDecoder(w.Body).Decode(&result))
+	assert.Equal(t, 0, len(result.Data))
 }
 
 // 任务3
-func testPatchCoupons(t *testing.T){
+func TestPatchCoupons(t *testing.T) {
 	
 }
