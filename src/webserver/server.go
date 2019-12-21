a8aae2d61528377ac7e83441716477b2f4a72d0c
晨 黎 <lichenlzc@outlook.com>
2019-12-21 09:17:42 +0800

    Your Message

--- src/webserver/server.go
+++ src/webserver/server.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
@@ -17,7 +20,7 @@ var mysql_client *sql.DB
 
 func init() {
 	redis_client = redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:16379",
+		Addr:     "127.0.0.1:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
@@ -28,7 +31,7 @@ func init() {
 		fmt.Println("Error open redis connection")
 		os.Exit(-1)
 	}
-	mysql_client, err = sql.Open("mysql", "root:123@tcp(127.0.0.1:13306)/projectdb")
+	mysql_client, err = sql.Open("mysql", "root:Lichen1996@tcp(127.0.0.1:3306)/projectdb")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
@@ -44,12 +47,12 @@ func init() {
 // var mysql_client =
 
 type Coupon struct {
-	Username    string
-	Coupons     string
-	Amount      int32
-	Stock       float32
-	Left        int32
-	Description string
+	Username    string  `json:"username"`
+	Coupons     string  `json:"name"`
+	Amount      int32   `json:"amount"`
+	Stock       float32 `json:"stock"`
+	Left        int32   `json:"left"`
+	Description string  `json:"description"`
 }
 
 type User struct {
@@ -91,28 +94,166 @@ func isUserExist(query_username string) bool {
 	}
 }
 
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+//任务2----------------------------------------------------------
+
+/*
+type CouponV2 struct {
+	Username string `json:"username"`
+
+	Coupons string `json:"name"`
+
+	Amount int32 `json:"amount"`
+
+	Stock float32 `json:"stock"`
+
+	Left int32 `json:"left"`
+
+	Description string `json:"description"`
+}
+
+func (c *CouponV2) ToCoupon() Coupon {
+	var cou Coupon
+	cou.username = c.Username
+	cou.coupons = c.Coupons
+	cou.amount = c.Amount
+	cou.stock = c.Stock
+	cou.left = c.Left
+	cou.description = c.Description
+	return cou
+}
+*/
+
+//将Coupon拼接成字符串，以#分隔：...#...#...
+func (c *Coupon) ToString() string {
+	var s string
+	s = fmt.Sprintf("%s#%s#%d#%f#%d#%s", c.Username, c.Coupons, c.Amount,
+		c.Stock, c.Left, c.Description)
+	return s
+}
+
+//将字符串转换成Coupon
+func (c *Coupon) ToCoupon(s string) {
+	j := strings.LastIndex(s, "#")
+	c.Description = s[j+1:]
+	s = s[:j]
+	j = strings.LastIndex(s, "#")
+	Left_coupons, _ := strconv.ParseInt(s[j+1:], 10, 32)
+	c.Left = int32(Left_coupons)
+	s = s[:j]
+	j = strings.LastIndex(s, "#")
+	Stock, _ := strconv.ParseFloat(s[j+1:], 32)
+	c.Stock = float32(Stock)
+	s = s[:j]
+	j = strings.LastIndex(s, "#")
+	Amount, _ := strconv.ParseInt(s[j+1:], 10, 32)
+	c.Amount = int32(Amount)
+	s = s[:j]
+	j = strings.LastIndex(s, "#")
+	c.Coupons = s[j+1:]
+	s = s[:j]
+	c.Username = s
+}
+
+// 任务2
+//check user if is business or not, exist or not
+//0:buniess, 1:customer, 2:not exit
+func checkUser(username string) int {
+	var user User_DB
+	err := mysql_client.QueryRow("SELECT username, password, kind FROM User WHERE username=?",
+		username).Scan(&user.Username, &user.Password, &user.Kind)
+	if err != nil { //未查询到的情况
+		return 2
+	} else if user.Kind == 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
 // 任务2
 func createCoupons(c *gin.Context) {
+	var couponJSON Coupon
+	err := c.BindJSON(&couponJSON)
+	couponJSON.Username = c.Param("username")
+	couponJSON.Left = couponJSON.Amount
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+	} else {
+		flag := checkUser(couponJSON.Username)
+		if flag == 2 {
+			c.JSON(400, gin.H{"errMsg": "不存在的商家"})
+		} else if flag == 1 {
+			c.JSON(400, gin.H{"errMsg": "非商家不能创建优惠券"})
+		} else {
+			mysql_client.Query("INSERT INTO Coupon (username, coupons, amount, stock, left_coupons, description) VALUES (?,?,?,?,?,?)",
+				couponJSON.Username, couponJSON.Coupons, couponJSON.Amount,
+				couponJSON.Stock, couponJSON.Left, couponJSON.Description)
+			c.JSON(201, gin.H{"errMsg": ""})
+		}
+	}
+}
 
+type GetCous struct {
+	ErrMsg string   `json:"errMsg"`
+	Data   []Coupon `json:"data"`
 }
 
 // 任务2
 func getCouponsInformation(c *gin.Context) {
-
+	Username := c.Param("username")
+	page := c.Query("page")
+	var resu GetCous
+	var cou Coupon
+	deviation, _ := strconv.Atoi(page)
+	query, _ := mysql_client.Query("SELECT username, coupons, amount, stock, left_coupons, description FROM Coupon WHERE username=? limit ?,20",
+		Username, (deviation-1)*20)
+	//	defer query.Close()
+	for query.Next() {
+		query.Scan(&cou.Username, &cou.Coupons, &cou.Amount,
+			&cou.Stock, &cou.Left, &cou.Description)
+		resu.Data = append(resu.Data, cou)
+	}
+	resultJSON, _ := json.Marshal(resu)
+	var result map[string]interface{}
+	err := json.Unmarshal(resultJSON, &result)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	c.JSON(200, result)
 }
 
 // 任务2
 func getCouponsFromRedis(Username string, cou string) (Coupon, error) {
-	return Coupon{}, nil
+	re, err := redis_client.Get(Username + "#" + cou).Result()
+	var result Coupon
+	if err == nil {
+		result.ToCoupon(re)
+	}
+	return result, err
 }
 
 // 任务2
 func setCouponsToRedis(Username string, cou Coupon) {
+	redis_client.Set(Username+"#"+cou.Coupons, cou.ToString(), 2*time.Second)
 }
 
 // 任务2
 func getCouponsFromRedisOrDatabase(Username string, cou string) (Coupon, error) {
-	return Coupon{}, nil
+	var result Coupon
+	result, err := getCouponsFromRedis(Username, cou)
+	if err != nil {
+		query, err := mysql_client.Query("SELECT * FROM Coupon WHERE Username=? AND Coupons=?", Username, cou)
+		if err == nil {
+			defer query.Close()
+			query.Next()
+			query.Scan(&result.Username, &result.Coupons, &result.Amount,
+				&result.Stock, &result.Left, &result.Description)
+			setCouponsToRedis(Username, result)
+		}
+	}
+	return result, err
 }
 
 // 任务3
@@ -160,6 +301,7 @@ func testValidateJWT(c *gin.Context) {
 	}
 }
 
+/*
 func main() {
 	// gin.SetMode(gin.ReleaseMode)
 	router := setupRouter()
@@ -167,5 +309,5 @@ func main() {
 	if err != nil {
 		fmt.Println("Error starting server")
 	}
-	fmt.Println("HAHAHA")
 }
+*/
